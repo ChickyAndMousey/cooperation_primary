daa4c969f3629b1c8c3db55a1bf8f3c12e7f40ec
Yujie_Huang <2436596349@qq.com>
2026-07-14 19:32:20 +0800

    设计程序框架和分工说明

---
+++ counter.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class counter { 
+    public static void main(String[] args) {
+        int count = 10;
+        int sum = 0;
+        // TODO
+
+        //
+        System.out.println("累加和为：" + sum);
+    }
+}
