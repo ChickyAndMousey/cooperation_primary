94c5078682d0426c1542a468ebe9d8c767f73948
Yujie_Huang <2436596349@qq.com>
2026-07-15 17:50:15 +0800

    完成

--- counter.java
+++ counter.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 public class counter { 
     public static void main(String[] args) {
-        int count = 10;
+        System.out.println("请输入数字：");
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
         int sum = 0;
         for (int i = 0;i <= count;i++) {
             sum = sum + i;
